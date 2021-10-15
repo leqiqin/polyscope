d393e98b8cc8156310f18428f6ab01778c004655
leqiqin <58313152+leqiqin@users.noreply.github.com>
2021-10-15 12:55:43 -0400

    Added interactivity

--- src/state.cpp
+++ src/state.cpp
@@ -1,6 +1,7 @@
 // Copyright 2017-2019, Nicholas Sharp and the Polyscope contributors. http://polyscope.run.
 #include "polyscope/polyscope.h"
 
+
 namespace polyscope {
 
 namespace state {
@@ -13,9 +14,23 @@ glm::vec3 center{0, 0, 0};
 std::map<std::string, std::map<std::string, Structure*>> structures;
 std::function<void()> userCallback = nullptr;
 
+
 // Lists of things
 std::set<Widget*> widgets;
 std::vector<SlicePlane*> slicePlanes;
+
+// ADDED for Ladybird
+size_t facePickIndStart;
+size_t edgePickIndStart;
+size_t halfedgePickIndStart;
+MeshSubset subset;
+int currVertexIndex = -1;
+int currFaceIndex = -1;
+int currEdgeIndex = -1;
+int deleteVertexIndex = -1;
+int deleteFaceIndex = -1;
+int deleteEdgeIndex = -1;
+std::function<void()> Show_Selected = nullptr;
 
 } // namespace state
 } // namespace polyscope
